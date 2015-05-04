f3aa4c8703f742931da76061d439ddeedf5e7b65
Chaitanya Khadilkar <ckhadilkar@enova.com>
2015-05-04 10:14:12 -0500

    Refactir- View in Blocks

--- cyoa/movie_review/app/helpers/static_pages_helper.rb
+++ cyoa/movie_review/app/helpers/static_pages_helper.rb
@@ -1,2 +1,13 @@
 module StaticPagesHelper
+	def guest_user(&block)
+		unless user_signed_in?
+			content = capture(&block)
+			concat content_tag(:div, content, class: 'center jumbotron')
+		end
+	end
+
+	def movie_posters(&block)
+		content = capture(&block)
+		concat content_tag(:div, content, class: 'center jumbotron')
+	end
 end
